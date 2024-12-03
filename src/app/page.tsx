ad5fe17e088efe031ba7c0240fb4299ec975ea35
You Young June <youngjune@planorban.com>
2024-12-03 01:47:40 -0500

    some ui fleshing out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,47 @@
 "use client";
 
 import { DataEntry } from "@/components/DataEntry";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { signIn, useSession } from "next-auth/react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-black text-white">
-      {session ? (
-        <DataEntry />
-      ) : (
-        <Button onClick={() => signIn("google")}>Login</Button>
-      )}
-    </main>
+    <div className="h-screen bg-black text-white">
+      <main className="flex h-[90%] items-center justify-center">
+        {session ? (
+          <DataEntry />
+        ) : (
+          <Button onClick={() => signIn("google")}>Login</Button>
+        )}
+      </main>
+      <footer className="flex h-[10%] justify-end pr-8">
+        {session ? (
+          <Popover>
+            <PopoverTrigger>
+              <Avatar>
+                <AvatarImage
+                  src={session ? String(session?.user?.image) : undefined}
+                  className="hover:cursor-pointer"
+                />
+                <AvatarFallback className="text-black">
+                  {Array.from(String(session?.user?.name))[0]}
+                </AvatarFallback>
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent className="bg-black border-slate-600 w-fit p-1">
+              <Button onClick={() => signOut()}>Sign Out</Button>
+            </PopoverContent>
+          </Popover>
+        ) : null}
+      </footer>
+    </div>
   );
 }
--- src/components/DataEntry.tsx
+++ src/components/DataEntry.tsx
@@ -1,6 +1,38 @@
-import { signOut } from "next-auth/react";
+"use client";
+
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { PlusIcon } from "@radix-ui/react-icons";
+import { Label } from "./ui/label";
+import { useState } from "react";
 
 export const DataEntry = () => {
-  return <Button onClick={() => signOut()}>Sign Out</Button>;
+  const [index, setIndex] = useState<number>(1);
+
+  return (
+    <div className="flex flex-col space-y-4">
+      {/* TODO: turn into data entry line component */}
+      <div className="flex space-x-4">
+        <div>
+          <Label htmlFor="test">Test</Label>
+          <Input id="test" />
+        </div>
+        <div>
+          <Label htmlFor="test2">Test 2</Label>
+          <Input id="test2" />
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <Button
+          className="w-fit p-2"
+          onClick={() => {
+            setIndex(index + 1);
+          }}
+        >
+          <PlusIcon />
+        </Button>
+      </div>
+      <Button>Submit</Button>
+    </div>
+  );
 };
