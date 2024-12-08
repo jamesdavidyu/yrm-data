876a0ab79174693376c543372de9f6dc744e3e10
You Young June <youngjune@planorban.com>
2024-12-08 03:05:41 -0500

    debugging authOptions

---
+++ src/app/api/auth/options.ts
@@ -0,0 +1,48 @@
+import { NextAuthOptions } from "next-auth";
+import Google from "next-auth/providers/google";
+import { getYrmApiHttpClient } from "@/lib/yrm-api-http-client";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async jwt({ token, account }) {
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+
+            return token;
+        },
+        async session({ session, token }) {
+            session.accessToken = token.accessToken;
+            return session;
+        },
+        async signIn({ user }) {
+            try {
+                const client = getYrmApiHttpClient();
+                const firstAndLastName = user.name?.split(" ");
+
+                const loginResponse = await client.loginUser({
+                    username: firstAndLastName ? firstAndLastName[0]+firstAndLastName[1] : "",
+                    password: user.email,
+                });
+
+                if (loginResponse.status > 300) {
+                    return false;
+                };
+
+                return true;
+            } catch (e) {
+                return false;
+            };
+        },
+    },
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import SessionProvider from "@/components/SessionProvider";
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "./api/auth/options";
 
 const inter = Inter({ subsets: ["latin"] });
 
